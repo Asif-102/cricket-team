b43ab60287b1a07fb2c92966c701231f8d341dbd
Asif2535 <aiqbal517@gmail.com>
2021-03-02 00:57:32 +0600

    make added player list

--- src/component/Main/Cart/Cart.js
+++ src/component/Main/Cart/Cart.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Cart.css';
 const Cart = (props) => {
-    const salary =  props.salary;
-    const name = props.name;
+    let salary =  props.salary;
+    let name = props.name;
     // console.log(name.length);
-    const totalSalary = salary.reduce((total,cost)=>total+cost,0);
-    return (
-        <div className="cart">
-            <h2>Total Selected Players: {name.length}</h2>
-            <h4>Added Players Name:</h4>
-            <p>{name}</p>
-            <h5>Total Selected Players Salary: <small>{totalSalary}{' '}</small>৳</h5>
-        </div>
-    );
+    let totalSalary=salary.reduce((total,cost)=>total+cost,0);
+    // console.log(newSalary);
+
+    let size = name.length;
+    let newName = name[size-1];
+    let minusName = name.slice(0,-1);
+    let addName = minusName.indexOf(newName);
+
+    // if(addName == -1){
+    //     name = props.name;
+        return (
+            <div className="cart">
+                <h2>Total Selected Players: {name.length}</h2>
+                <h4>Added Players Name:</h4>
+                <ol>
+                    {
+                        name.map(player=><li>{player}</li>)
+                    }
+                </ol>
+                <h5>Total Selected Players Salary: <small>{totalSalary}{' '}</small>৳</h5>
+            </div>
+        )
+    // }
+    // else{
+    //     return alert('already added');
+    // }
 };
 
 export default Cart;
